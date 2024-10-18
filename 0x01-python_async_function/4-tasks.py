61e84e3314df929937a5e77d423762761329bed8
chima-01 <divinechima76@gmail.com>
2024-10-18 05:14:01 -0700

    performed task according to instructions

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -5,9 +5,15 @@ The code is nearly identical to wait_n except task_wait_random called.
 """
 import asyncio
 from typing import List
-wait_n = __import__('1-concurrent_coroutines').wait_n
+task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    task = await wait_n(n, max_delay)
-    return task
+    """
+    Args:
+        n - number of times to call coroutine
+        max_delay - Delay time
+    """
+    task = [task_wait_random(max_delay) for _ in range(n)]
+    all_task = await asyncio.gather(*task)
+    return sorted(all_task)
