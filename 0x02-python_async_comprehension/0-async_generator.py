c900f673ea733410106966a9e57b88c8417d96df
chima-01 <divinechima76@gmail.com>
2024-10-18 14:37:58 -0700

    add generator annotation

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -6,9 +6,10 @@
 """
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     """
     arg: no arg return random floating number
     """
